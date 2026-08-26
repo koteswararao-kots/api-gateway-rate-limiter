6f60b769f9c1e1d5186688573df03849256a043a
koteswar-kots <kotsmaddhur@gmail.com>
2026-08-26 20:20:44 +0530

    add total request analytics

--- gateway/src/config/redis.js
+++ gateway/src/config/redis.js
@@ -3,9 +3,18 @@ require('dotenv').config()
 const redisClient = createClient({
   url: process.env.REDIS_URL
 })
+console.log('REDIS URL:', process.env.REDIS_URL);
 
 redisClient.on('error', (error) => {
-  console.error('Redis Client Error:', error)
-})
+  console.error('Redis Client Error:', error);
+});
+
+redisClient.on('connect', () => {
+  console.log('Redis socket connected');
+});
+
+redisClient.on('ready', () => {
+  console.log('Redis client ready');
+});
 
 module.exports = redisClient;
--- gateway/src/middleware/requestLogger.js
+++ gateway/src/middleware/requestLogger.js
@@ -1,25 +1,35 @@
-const requestLogger = (req, res, next) => {
-  const startTime = Date.now()
+const redisClient = require('../config/redis');
 
-  res.on('finish', () => {
+const requestLogger = (req, res, next) => {
+
+  const startTime = Date.now();
+
+  res.on('finish', async () => {
+
     const responseTime = Date.now() - startTime;
-    const method = req.method
-    const endpoint = req.originalUrl
-    const ip = req.ip
+    const method = req.method;
+    const endpoint = req.originalUrl;
+    const ip = req.ip;
     const statusCode = res.statusCode;
     const userId = req.user?.userId || 'anonymous';
+
+   await redisClient.incr('analytics:total_requests');
+    console.log(db);
     const log = {
       method,
       endpoint,
       ip,
       userId,
       statusCode,
-      responseTime, 
-    } 
-    console.log(log)
-  })
+      responseTime,
+    };
+
+    console.log(log);
+  });
+
   next();
-}
+};
+
 module.exports = {
   requestLogger
-}
+};
