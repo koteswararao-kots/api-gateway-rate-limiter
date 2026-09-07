b2a5d47633e3af3dee4686a782a923c2a58329c4
koteswar-kots <kotsmaddhur@gmail.com>
2026-09-07 12:00:06 +0530

    security changes

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -20,11 +20,15 @@ const { cache } = require('./middleware/cache');
 
 const analyticsRoutes = require('./modules/analytics/routes');
 const { register } = require('./metrics/metrics');
+const helmet = require('helmet');
+const cors = require('cors');
 
 
 const app = express();
 
-app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(express.json({limit: '1mb'}));
 
 app.use(requestLogger);
 
